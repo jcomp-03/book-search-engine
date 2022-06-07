bf8f5a969e7d82d8a38229b7d5f750c5b29c2a06
jcomp-03@James-Desktop <jcomp_03@yahoo.com>
2022-06-06 22:20:41 -0400

    Committing changes.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,51 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    _id: ID
+    bookId: String
+    auhtors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInfo {
+    author: [String]
+    description: String
+    title: String
+    bookId: String
+    url: String
+    image: String
+  }
+
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookToSave: BookInfo!): User
+    removeBook(bookId: ID!): User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
---
+++ server/server.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const path = require("path");
+const db = require("./config/connection");
+const routes = require("./routes");
+
+const app = express();
+const PORT = process.env.PORT || 3002;
+
+// import ApolloServer
+const { ApolloServer } = require("apollo-server-express");
+// import middleware function which verifies the provided JWT
+const { authMiddleware } = require('./utils/auth');
+
+// we need to import our typeDefs and resolvers here.
+// the path may need changing...
+const { typeDefs, resolvers } = require("./schemas");
+
+// create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
+}
+
+app.use(routes);
+
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async (typeDefs, resolvers) => {
+  await server.start();
+  // integrate our Apollo server with the Express application as middleware
+  server.applyMiddleware({ app });
+
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      // log where we can go to test our GQL API
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+// Call the async function to start the server
+startApolloServer(typeDefs, resolvers);
